e31c12145bd449eef12408cb3862ae666079c70e
Anthony <anthonygibson24@gmail.com>
2022-06-24 22:16:51 -0500

    material UI test

---
+++ src/components/LoginForm.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+    Paper,
+    FormControl,
+    InputLabel,
+    OutlinedInput,
+    Button
+} from '@material-ui/core';
+const styles = {
+    paper: {
+        width: "20rem", padding: "1rem"
+    },
+    input: {
+        marginBottom: "1rem"
+    },
+    button: {
+        width: "100%"
+    }
+}
+const LoginForm = () => {
+    return (
+        <div>
+            <form>
+                <label>test</label>
+                <input></input>
+            </form>
+        </div>
+    )
+
+}
+export default LoginForm;
+
